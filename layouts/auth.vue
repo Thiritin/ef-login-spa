5cb3963fd2df6a408c039493203bc5c80e60f8d8
Thiritin <me@thiritin.com>
2020-08-22 22:42:31 +0200

    Remove Consent

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -4,7 +4,7 @@
       <img src="~/assets/bg-logo.svg" alt="Background Logo" />
     </div>
     <div
-      class="container px-4 sm:px-12 pt-16 pb-12 mx-auto flex items-center md:min-h-screen"
+      class="container px-4 sm:px-12 pt-16 pb-12 mx-auto flex items-center md:min-h-screen md:-mt-12"
       style="max-width:600px"
     >
       <nuxt />
--- pages/consent.vue
+++ pages/consent.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="w-full">
     <Logo></Logo>
-    <LoginScreenWelcome class="mb-3" title="Action Required" sub-title="An application requests access to your Data!"></LoginScreenWelcome>
-    <div class="border-b border-gray-200 mb-6"></div>
+    <LoginScreenWelcome class="mb-6" title="Grant Permission" sub-title="Eurofurence Registration would like to access the following Data:"></LoginScreenWelcome>
     <div class="ml-4 mb-2 flex items-center">
       <div class="bg-gray-200 rounded p-2 inline-block">
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="paw" class="svg-inline--fa fa-paw fa-w-16 w-6 h-6 text-primary-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 224c-79.41 0-192 122.76-192 200.25 0 34.9 26.81 55.75 71.74 55.75 48.84 0 81.09-25.08 120.26-25.08 39.51 0 71.85 25.08 120.26 25.08 44.93 0 71.74-20.85 71.74-55.75C448 346.76 335.41 224 256 224zm-147.28-12.61c-10.4-34.65-42.44-57.09-71.56-50.13-29.12 6.96-44.29 40.69-33.89 75.34 10.4 34.65 42.44 57.09 71.56 50.13 29.12-6.96 44.29-40.69 33.89-75.34zm84.72-20.78c30.94-8.14 46.42-49.94 34.58-93.36s-46.52-72.01-77.46-63.87-46.42 49.94-34.58 93.36c11.84 43.42 46.53 72.02 77.46 63.87zm281.39-29.34c-29.12-6.96-61.15 15.48-71.56 50.13-10.4 34.65 4.77 68.38 33.89 75.34 29.12 6.96 61.15-15.48 71.56-50.13 10.4-34.65-4.77-68.38-33.89-75.34zm-156.27 29.34c30.94 8.14 65.62-20.45 77.46-63.87 11.84-43.42-3.64-85.21-34.58-93.36s-65.62 20.45-77.46 63.87c-11.84 43.42 3.64 85.22 34.58 93.36z"></path></svg>
@@ -17,7 +16,7 @@
     </div>
     <div class="grid gap-3 grid-cols-2">
       <a href="#" class="w-full text-center rounded bg-gray-300 py-4 text-xl font-semibold text-gray-800">Decline</a>
-      <a href="#" class="w-full text-center rounded bg-primary-800 py-4 text-xl font-semibold text-primary-100">Accept</a>
+      <a href="#" class="w-full text-center rounded bg-primary-700 py-4 text-xl font-semibold text-primary-100">Accept</a>
     </div>
   </div>
 </template>
